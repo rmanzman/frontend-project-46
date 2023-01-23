8305d53e389cca1a0a136751b2b42ec3f62513b8
Roman Kraizman <roman.krayzman@gmail.com>
2023-01-23 23:55:33 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,7 +7,7 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 // const formats = ['json', 'yaml', 'yml'];
 const formats = ['json'];
@@ -16,9 +16,9 @@ describe('genDiff should work correctly', () => {
   test.each(formats)('genDiff should work with %p', (format) => {
     const filepath1 = getFixturePath(`file1.${format}`);
     const filepath2 = getFixturePath(`file2.${format}`);
-    expect(genDiff(filepath1, filepath2)).toBe(readFile('expectedStylish.txt'));
-    // expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
-    // expect(genDiff(filepath1, filepath2, 'plain')).toEqual(readFile('expectedPlain.txt'));
-    // expect(genDiff(filepath1, filepath2, 'json')).toEqual(readFile('expectedJSON.txt'));
+    expect(genDiff(filepath1, filepath2)).toEqual(readFixture('expectedStylish.txt'));
+    // expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(readFixture('expectedStylish.txt'));
+    // expect(genDiff(filepath1, filepath2, 'plain')).toEqual(readFixture('expectedPlain.txt'));
+    // expect(genDiff(filepath1, filepath2, 'json')).toEqual(readFixture('expectedJSON.txt'));
   });
 });
