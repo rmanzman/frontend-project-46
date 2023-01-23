c3fe08e1090ea420a9a1e6d69afa96498799d5ee
Roman Kraizman <roman.krayzman@gmail.com>
2023-01-23 19:49:44 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,12 +9,13 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const formats = ['json', 'yaml', 'yml'];
+// const formats = ['json', 'yaml', 'yml'];
+const formats = ['json'];
 
 describe('genDiff should work correctly', () => {
   test.each(formats)('genDiff should work with %p', (format) => {
-    const filepath1 = getFixturePath(`nested1.${format}`);
-    const filepath2 = getFixturePath(`nested2.${format}`);
+    const filepath1 = getFixturePath(`file1.${format}`);
+    const filepath2 = getFixturePath(`file2.${format}`);
     expect(genDiff(filepath1, filepath2)).toEqual(readFile('expectedStylish.txt'));
     expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
     // expect(genDiff(filepath1, filepath2, 'plain')).toEqual(readFile('expectedPlain.txt'));
