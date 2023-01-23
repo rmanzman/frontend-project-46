987a209883651fe6075bb71fd50f1b234a70dfb8
Roman Kraizman <roman.krayzman@gmail.com>
2023-01-23 23:59:12 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,15 +10,25 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 // const formats = ['json', 'yaml', 'yml'];
-const formats = ['json'];
+// const formats = ['json'];
 
-describe('genDiff should work correctly', () => {
-  test.each(formats)('genDiff should work with %p', (format) => {
-    const filepath1 = getFixturePath(`file1.${format}`);
-    const filepath2 = getFixturePath(`file2.${format}`);
-    expect(genDiff(filepath1, filepath2)).toEqual(readFixture('expectedStylish.txt'));
-    // expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(readFixture('expectedStylish.txt'));
-    // expect(genDiff(filepath1, filepath2, 'plain')).toEqual(readFixture('expectedPlain.txt'));
-    // expect(genDiff(filepath1, filepath2, 'json')).toEqual(readFixture('expectedJSON.txt'));
-  });
+const expectedResult1 = readFixture('expectedStylish.txt');
+
+test('JSON', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+
+  expect(genDiff(filepath1, filepath2)).toEqual(expectedResult1);
+  expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedResult1);
 });
+
+// describe('genDiff should work correctly', () => {
+//   test.each(formats)('genDiff should work with %p', (format) => {
+//     const filepath1 = getFixturePath(`file1.${format}`);
+//     const filepath2 = getFixturePath(`file2.${format}`);
+//     expect(genDiff(filepath1, filepath2)).toEqual(readFixture('expectedStylish.txt'));
+//     expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(readFixture('expectedStylish.txt'));
+//     expect(genDiff(filepath1, filepath2, 'plain')).toEqual(readFixture('expectedPlain.txt'));
+//     expect(genDiff(filepath1, filepath2, 'json')).toEqual(readFixture('expectedJSON.txt'));
+//   });
+// });
