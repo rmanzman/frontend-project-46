addb0704c57f7973405937beaedf99df6d386ee8
Roman Kraizman <roman.krayzman@gmail.com>
2023-01-24 23:28:18 +0300

    update

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,27 +1,45 @@
-const iter = (diff) => diff.map((node) => {
+import _ from 'lodash';
+
+const IS_FULL = true;
+const spacesCount = 4;
+const replacer = ' ';
+
+const indent = (depth, isFull = false) => {
+  const indentSize = depth * spacesCount;
+  return isFull ? replacer.repeat(indentSize) : replacer.repeat(indentSize - 2);
+};
+
+const stringify = (data, depth) => {
+  if (!_.isPlainObject(data)) {
+    return String(data);
+  }
+  const lines = Object
+    .entries(data)
+    .map(([key, value]) => `${indent(depth + 1, IS_FULL)}${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${indent(depth, IS_FULL)}}`;
+};
+
+const iter = (diff, depth = 1) => diff.map((node) => {
   switch (node.flag) {
     case 'deleted':
-      return `  - ${node.key}: ${node.value}`;
+      return `${indent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
     case 'added':
-      return `  + ${node.key}: ${node.value}`;
-    case 'changed':
-      return `  - ${node.key}: ${node.value1}\n  + ${node.key}: ${node.value2}`;
+      return `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
+    case 'changed': {
+      return `${indent(depth)}- ${node.key}: ${stringify(node.value1, depth)}\n${indent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
+    }
     case 'unchanged':
-      return `    ${node.key}: ${node.value}`;
+      return `${indent(depth, IS_FULL)}${node.key}: ${stringify(node.value, depth)}`;
+    case 'nested': {
+      const lines = iter(node.children, depth + 1);
+      return `${indent(depth, IS_FULL)}${node.key}: {\n${lines.join('\n')}\n${indent(depth, IS_FULL)}}`;
+    }
     default:
       throw new Error(`Unknown type of node ${node.type}.`);
   }
 });
 
-const buildLinesOutput = (diff) => {
-  return [
-    '{',
-    ...diff,
-    '}',
-  ].join('\n');
-};
-
-export default (diff) => {
-  const result = iter(diff);
-  return buildLinesOutput(result);
+export default (tree) => {
+  const result = iter(tree, 1);
+  return `{\n${result.join('\n')}\n}`;
 };
