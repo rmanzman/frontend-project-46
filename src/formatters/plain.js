3555c8a014dd85c22abb89551a6b7e82b7896112
Roman Kraizman <roman.krayzman@gmail.com>
2023-01-27 01:06:48 +0300

    added plain.js

---
+++ src/formatters/plain.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+
+const stringify = (value) => {
+  if (_.isObjectLike(value)) {
+    return '[complex value]';
+  }
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  return String(value);
+};
+
+const buildPath = (node, currentPath) => (currentPath !== '' ? `${currentPath}.${node.key}` : String(node.key));
+
+const iter = (tree, path) => tree
+  .filter((node) => node.flag !== 'unchanged')
+  .map((node) => {
+    const currentPath = buildPath(node, path);
+    switch (node.flag) {
+      case 'added':
+        return `Property '${currentPath}' was added with value: ${stringify(node.value)}`;
+      case 'deleted':
+        return `Property '${currentPath}' was removed`;
+      case 'changed': {
+        return `Property '${currentPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+      }
+      case 'nested':
+        return iter(node.children, currentPath).join('\n');
+      default:
+        throw new Error(`Unknown node type ${node.flag}.`);
+    }
+  });
+
+export default (tree) => iter(tree, '').join('\n');
