e32ce95b57905be9906f8c6a2ac7c738d2c97dc8
Roman Kraizman <roman.krayzman@gmail.com>
2023-01-31 02:19:46 +0300

    refactoring for 'spaces' functions

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,12 +1,16 @@
 import _ from 'lodash';
 
-const IS_FULL = true;
 const spacesCount = 4;
 const replacer = ' ';
 
-const indent = (depth, isFull = false) => {
+const twoOrSixSpaces = (depth) => {
   const indentSize = depth * spacesCount;
-  return isFull ? replacer.repeat(indentSize) : replacer.repeat(indentSize - 2);
+  return replacer.repeat(indentSize - 2);
+};
+
+const fourOrEightSpaces = (depth) => {
+  const indentSize = depth * spacesCount;
+  return replacer.repeat(indentSize);
 };
 
 const stringify = (data, depth) => {
@@ -15,28 +19,28 @@ const stringify = (data, depth) => {
   }
   const lines = Object
     .entries(data)
-    .map(([key, value]) => `${indent(depth + 1, IS_FULL)}${key}: ${stringify(value, depth + 1)}`);
-  return `{\n${lines.join('\n')}\n${indent(depth, IS_FULL)}}`;
+    .map(([key, value]) => `${fourOrEightSpaces(depth + 1)}${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${fourOrEightSpaces(depth)}}`;
 };
 
 const iter = (diff, depth = 1) => diff
   .map((node) => {
     switch (node.type) {
       case 'deleted':
-        return `${indent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
+        return `${twoOrSixSpaces(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
       case 'added':
-        return `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
+        return `${twoOrSixSpaces(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
       case 'changed': {
-        return `${indent(depth)}- ${node.key}: ${stringify(node.value1, depth)}\n${indent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
+        return `${twoOrSixSpaces(depth)}- ${node.key}: ${stringify(node.value1, depth)}\n${twoOrSixSpaces(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
       }
       case 'unchanged':
-        return `${indent(depth, IS_FULL)}${node.key}: ${stringify(node.value, depth)}`;
+        return `${fourOrEightSpaces(depth)}${node.key}: ${stringify(node.value, depth)}`;
       case 'nested': {
         const lines = iter(node.children, depth + 1);
-        return `${indent(depth, IS_FULL)}${node.key}: {\n${lines.join('\n')}\n${indent(depth, IS_FULL)}}`;
+        return `${fourOrEightSpaces(depth)}${node.key}: {\n${lines.join('\n')}\n${fourOrEightSpaces(depth)}}`;
       }
       default:
-        throw new Error(`Unknown type of node ${node.type}.`);
+        throw new Error(`Unknown type of node '${node.type}'.`);
     }
   });
 
